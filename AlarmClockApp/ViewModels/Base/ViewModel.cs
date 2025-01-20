bc99af42e98239b0b326b639104ed8477d892c92
Reyme87 <astafev.ivan.0505@mail.ru>
2025-01-20 16:13:41 +0300

    Добавление реализации интерфейса IDisposable

--- AlarmClockApp/ViewModels/Base/ViewModel.cs
+++ AlarmClockApp/ViewModels/Base/ViewModel.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace AlarmClockApp.ViewModels.Base
 {
-    internal abstract class ViewModel : INotifyPropertyChanged
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -23,6 +23,21 @@ namespace AlarmClockApp.ViewModels.Base
             field = value;
             OnPropertyChanged(PropertyName);
             return true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        
+        private bool _disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing || _disposed)
+            {
+                return;
+            }
+            _disposed = true;
         }
     }
 }
