00fe0fde3638b8f7648b9a8a107a55d89d1f7ca8
Reyme87 <astafev.ivan.0505@mail.ru>
2025-01-24 13:16:03 +0300

    Создание базового класса Command

---
+++ AlarmClockApp/Commands/Base/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace AlarmClockApp.Commands.Base
+{
+    internal class Command : ICommand
+    {
+
+    }
+}
