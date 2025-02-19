7b53c6c91383e740e6cf81990948a7b8d930f628
Reyme87 <astafev.ivan.0505@mail.ru>
2025-02-19 14:39:27 +0300

    Создание модели DatePreset

---
+++ AlarmClockApp/Models/DatePreset.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmClockApp.Models
+{
+    internal class DatePreset
+    {
+        private string _hour;
+        private string _minute;
+        private DateTime _date;
+
+        public DatePreset(string hour, string minute, DateTime date)
+        {
+            _hour = hour;
+            _minute = minute;
+            _date = date;
+        }
+
+        public string Hour
+        {
+            get => _hour;
+        }
+
+        public string Minute
+        {
+            get => _minute;
+        }
+
+        public DateTime Date
+        {
+            get => _date;
+        }
+    }
+}
