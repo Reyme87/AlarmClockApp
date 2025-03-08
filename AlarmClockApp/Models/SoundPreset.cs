6e094e36b4c1d5a717500c155a4a9b93fbc3794a
Reyme87 <astafev.ivan.0505@mail.ru>
2025-03-08 14:48:44 +0300

    Создание модели SoundPreset

---
+++ AlarmClockApp/Models/SoundPreset.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AlarmClockApp.Models
+{
+    class SoundPreset
+    {
+        private string _path;
+        private string _length;
+        private string _name;
+
+        public SoundPreset(string path, string length, string name)
+        {
+            _path = path;
+            _length = length;
+            _name = name;
+        }
+
+        public string Path
+        {
+            get => _path;
+        }
+
+        public string Length
+        {
+            get => _length;
+        }
+
+        public string Name
+        {
+            get => _name;
+        }
+    }
+}
